e267894d4e58e339639cf7df344a8cc6e36d6cea
Inocencio Cordeiro Armando <34503843+InocencioC@users.noreply.github.com>
2025-06-08 02:35:16 +0100

    feat: docker  compose configured.

---
+++ src/main/java/com/ecommerce/ecommerce_event_producer/EcommerceEventProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_event_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceEventProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceEventProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecommerce_event_producer/EcommerceEventProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_event_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceEventProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
